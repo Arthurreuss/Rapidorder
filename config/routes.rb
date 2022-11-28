4b8b0d064e7282ef6caf62a27c76389e71735a26
Jorrit van der Plaats <jvdplaats@hotmail.com>
2022-11-28 16:34:53 +0100

    add controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
+  get 'orders/index'
+  get 'orders/show'
+  get 'orders/create'
+  get 'orders/update'
+  get 'orders/destroy'
+  get 'restaurants/show'
+  get 'restaurants/new'
+  get 'restaurants/create'
+  get 'restaurants/edit'
+  get 'restaurants/update'
+  get 'restaurants/destroy'
+  get 'products/index'
+  get 'products/new'
+  get 'products/create'
+  get 'products/edit'
+  get 'products/update'
+  get 'products/destroy'
+  get 'reviews/create'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
