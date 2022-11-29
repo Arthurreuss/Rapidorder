cbf8b7e8c7844e5e0a151e03b9a27401bec3757d
Jorrit van der Plaats <jvdplaats@hotmail.com>
2022-11-29 11:18:58 +0100

    create restuarant show

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,12 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
   has_many :products
-  
+
   validates :name, presence: true
   validates :description, presence: true
   validates :address, presence: true
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
 end
