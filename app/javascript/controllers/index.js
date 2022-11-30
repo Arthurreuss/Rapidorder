12f5f04957549d49d0582f748a75c219fe16e3ea
Arthur Reuss <arthur.reuss@gmx.de>
2022-11-30 18:07:19 +0100

    master push

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -10,11 +10,5 @@ import { application } from "./application"
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
-
-import SelectCategoryController from "./select_category_controller"
-application.register("select-category", SelectCategoryController)
-
-
 import CounterController from "./counter_controller"
 application.register("counter", CounterController)
-
