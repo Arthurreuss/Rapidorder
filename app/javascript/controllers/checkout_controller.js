92fe138cbaa5dcd0f426d4ab0cea6997970198ce
Andres Ferrer <andferrer6a@gmail.com>
2022-12-01 12:54:04 +0100

    checkout

--- app/javascript/controllers/checkout_controller.js
+++ app/javascript/controllers/checkout_controller.js
@@ -1,8 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
+import { loadScript } from "@paypal/paypal-js"
 
 // Connects to data-controller="checkout"
 export default class extends Controller {
   connect() {
-    console.log("hello")
+    loadScript({"client-id":"AZ6mg8z-fSKBI8oMSU29gsgDxzw65DUC1lC0VQ5Yq-d-u8sz8rWiTv0wENaimMPhF4x-BRWVoGpZ49Cg"}).then((paypal) => {
+      paypal.Buttons().render('#paypal-button-container')
+    })
   }
 }
