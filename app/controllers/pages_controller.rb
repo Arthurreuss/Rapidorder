e0df124b09d6ec013297f6302e3247f30530fe46
root <leonidastaousanis76@gmail.com>
2022-12-01 16:04:46 +0100

    pameeeee omadaraaaa

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,6 +14,10 @@ class PagesController < ApplicationController
     @tables = @tables.select { |table| table.orders.present? }
   end
 
+  def cart
+    @products = Product.all
+  end
+
   def generate_qr_codes
     @restaurant = Restaurant.find(params[:restaurant_id])
     @array_qr_codes = []
