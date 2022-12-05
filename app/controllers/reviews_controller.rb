47be17bef890a8e30cf1098bcd23a10543072e07
Andres Ferrer <andferrer6a@gmail.com>
2022-12-05 18:05:47 +0100

    reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,37 @@
 class ReviewsController < ApplicationController
+  def new
+    @product = Product.find(params[:product_id])
+    @review = Review.new
+  end
+
   def create
+    @product = Product.find(params[:product_id])
+    @review = Review.new(review_params)
+    @review.product = @product
+    if @review.save
+      format.html { redirect_to restaurants_path, notice: "Review successfully created" }
+    else
+      format.html { render :new, status: :unprocessable_entity }
+    end
+  end
+
+  def render_confirmation
+  confirmation = params['_json']
+
+  cart.map! do |order|
+    { product: Product.find(product[:id]),
+    amount: order[:amount] }
+  end
+  respond_to do |format|
+    format.text {
+      render partial: "shared/confirmation", locals: {cart: cart}, formats: [:html]
+    }
+  end
+end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating)
   end
 end
---
+++ app/javascript/controllers/confirmation_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="confirmation"
+export default class extends Controller {
+  static targets = ["card"];
+
+  connect() {
+    console.log("connected");
+    const csrfToken = document.querySelector("[name='csrf-token']").content;
+    fetch("/confirmation", {
+      method: "POST",
+      headers: {
+        Accept: "text/plain",
+        "X-CSRF-Token": csrfToken,
+        "Content-Type": "application/json",
+      },
+      body: localStorage.getItem("cart"),
+    })
+      .then((response) => response.text())
+      .then((data) => {
+        this.cardTarget.innerHTML = data;
+      });
+  }
+  }
