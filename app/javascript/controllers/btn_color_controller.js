05e8d8ad9f5297eb0d153ab1e0b4fa3578d2d937
root <leonidastaousanis76@gmail.com>
2022-12-06 09:12:08 +0100

    fuck

---
+++ app/javascript/controllers/btn_color_controller.js
@@ -0,0 +1,21 @@
+// import { Controller } from "@hotwired/stimulus"
+
+// // Connects to data-controller="btn-color"
+// export default class extends Controller {
+//   connect() {
+//   console.log("we are conected")
+//   }
+//   const btn = document.getElementById('btn-color');
+
+//   let index = 0;
+
+//   const colors = ['salmon', 'green', 'blue', 'purple'];
+
+//   btn.addEventListener('click', function onClick() {
+//     btn.style.backgroundColor = colors[index];
+//     btn.style.color = 'white';
+
+//     index = index >= colors.length - 1 ? 0 : index + 1;
+//   });
+
+// }
